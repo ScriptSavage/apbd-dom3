f59d66a1c173fe9182bf1d1e6666f1e8b7f6d8a6
MAX <maksymiliangaleziowski@gmail.com>
2024-04-06 15:25:56 +0200

    modyfikacja

---
+++ zadanie/LegacyAppTests/UserServiceTest.cs
@@ -0,0 +1,42 @@
+using LegacyApp;
+
+namespace LegacyAppTests;
+
+public class UserServiceTest
+{
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Is_Empty()
+    {
+        // Arrange
+        var service = new UserService();
+        string firstName = "";
+        string lastName = "Doe";
+        DateTime birthDate = new DateTime(1980, 1, 1);
+        int clientId = 1;
+        string email = "john.doe@example.com";
+
+        // Act
+        bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_Emial_Without_At_And_Dot()
+    {
+        //Arange
+        string firstName = "John";
+        string lastName = "Doe";
+        DateTime birthDate = new DateTime(1980, 1, 1);
+        int clientId = 1;
+        string email = "doe";
+        var service = new UserService();
+
+        //Act
+        bool result = service.AddUser(firstName, lastName, email , birthDate,clientId);
+        
+        //Assert
+        Assert.Equal(false,result);
+    }
+}
